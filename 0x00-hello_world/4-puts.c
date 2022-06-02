8f6bbd420a8470a17d7668662912a0ae599bbaf7
5hisia <edu.osteen@gmail.com>
2022-06-02 14:06:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * print_hello - Prints "Hello"
+ * main - Prints "Programming is like building a multilingual puzzle"
  */
 int main(void)
 {
 
       puts("\"Programming is like building a multilingual puzzle"); 
+
       return (0);
 }
