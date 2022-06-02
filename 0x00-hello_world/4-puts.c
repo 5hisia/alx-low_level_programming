d4f024c3dd9de508e050e3430196d244890502dc
5hisia <edu.osteen@gmail.com>
2022-06-02 14:02:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 /**
  * print_hello - Prints "Hello"
  */
- int main(void)
-  {
-   puts("\"Programming is like building a multilingual puzzle"); 
-   return (0); 
-  }
+int main(void)
+{
+
+      puts("\"Programming is like building a multilingual puzzle"); 
+      return (0);
+}
