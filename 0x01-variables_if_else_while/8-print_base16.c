a4b7676f6049f806e321ec65af03a3cc900de0c0
5hisia <edu.osteen@gmail.com>
2022-06-03 13:27:20 -0700

    m

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,13 +3,11 @@
 
 
 /**
+ *main - prints all the numbers of base 16 in lowercase,
  *
- *  * main - prints all the numbers of base 16 in lowercase,
+ *followed by a new line
  *
- *   * followed by a new line
- *
- *    * Return: Always 0 (Success)
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
