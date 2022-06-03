ee11c6664a684172ea234f13b084aecc1c3326dd
5hisia <edu.osteen@gmail.com>
2022-06-03 12:10:58 -0700

    m

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,14 +3,10 @@
 
 
 /**
-
- * main - prints the alphabet in lowercase, and then in uppercase,
-
- * followed by a new line
-
- * Return: Always 0 (Success)
-
- */
+*main - prints the alphabet in lowercase, and then in uppercase,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
 
 int main(void)
   
@@ -19,7 +15,7 @@ int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
 for (ch = 'A'; ch <= 'Z'; ch++)
-putchar(ch);  
+putchar(ch);
 putchar('\n');
-return (0); 
+return (0);
 }
