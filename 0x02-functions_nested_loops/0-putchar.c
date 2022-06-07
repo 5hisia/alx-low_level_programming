70e003569b06ee2271b3d00af61f4664ec21e467
5hisia <edu.osteen@gmail.com>
2022-06-07 13:38:35 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,20 @@
 #include "main.h"
 /**
  * main - main fuctions 
- * Reaturn: always return 0
+ *
+ * Return: always return 0
  *
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return(0);
 }
