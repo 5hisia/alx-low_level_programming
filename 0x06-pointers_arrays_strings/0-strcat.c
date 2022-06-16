cf415de4fb626e142bab4994f5eede5f7326e55d
5hisia <edu.osteen@gmail.com>
2022-06-16 13:25:46 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - function commute string 
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return vale of dest 
+ */
+
+char *strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest0);
+}
+
